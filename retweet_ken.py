78acf7871882c4fe0490787c8947f6e021c712f2
otv0604 <smallassassin@hotmail.co.jp>
2023-05-25 21:58:46 +0900

    rev0.05 get tweet by scroll

--- retweet_ken.py
+++ retweet_ken.py
@@ -23,40 +23,35 @@ try:
         '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div[2]',
     ).click()
 
-    # 最初のツイート要素を取得
-    previous_tweets = driver.find_elements(By.CSS_SELECTOR, "div[data-testid='tweet']")
-
     for _ in range(3):
-        # 1000pxスクロールする
-        driver.execute_script("window.scrollBy(0, 1000);")
-        print("スクロールスクロールスクロールスクロール出ていけ")  # スクロール部分のコメントに「出ていけ」を追加
-        print("スクロールスクロールスクロールスクロール出ていけ")
-        print("スクロールスクロールスクロールスクロール出ていけ")
-
         # ページのロード完了を待機
         wait.until(EC.visibility_of_element_located((By.TAG_NAME, "article")))
 
         # 新しく表示されたツイート要素のみを取得
         current_tweets = driver.find_elements(By.TAG_NAME, "article")
-        new_tweets = [tweet for tweet in current_tweets if tweet not in previous_tweets]
 
-        for tweet in new_tweets:
+        for tweet in current_tweets:
             try:
                 print("==============================")
-                print(tweet.text + "出ていけ")  # コメントの語尾に「出ていけ」を追加
+                print(tweet.text + "出ていけ")
                 print("==============================")
                 with open("tweets.txt", "a", encoding="utf-8") as file:
                     text = tweet.text.strip()
-                if text:
-                    file.write("出ていけ\n")
-                    file.write(text + "\n")
-                    file.write("出ていけ\n")
+                    if text:
+                        file.write("*****************************\n")
+                        file.write(text + "\n")
+                        file.write("*****************************\n")
             except Exception as e:
                 print(f"An error occurred: {str(e)}")
-            continue
+                continue
 
-    # 新しく表示されたツイートを前回のツイートとして更新
-    previous_tweets = current_tweets
+        # 1000pxスクロールする
+        driver.execute_script("window.scrollBy(0, 2000);")
+        print("スクロールスクロールスクロールスクロール出ていけ")
+        print("スクロールスクロールスクロールスクロール出ていけ")
+        print("スクロールスクロールスクロールスクロール出ていけ")
+
+        time.sleep(1)
 
 finally:
     driver.quit()
