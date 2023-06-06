c775762e4d51c79530b5b1ef240a7799a2ef2471
otv0604 <smallassassin@hotmail.co.jp>
2023-06-07 00:06:25 +0900

    rev0.12 stale element error in loop2

--- retweet_ken.py
+++ retweet_ken.py
@@ -34,76 +34,97 @@ driver.get(web_url)  # クッキーを設定した後またアクセス
 WebDriverWait(driver, 10).until(
     EC.presence_of_all_elements_located((By.TAG_NAME, "article"))
 )
-tweets = driver.find_elements(By.TAG_NAME, "article")
+# tweets = driver.find_elements(By.TAG_NAME, "article")
 
-for tweet in tweets:
+for _ in range(5):
     tweets = driver.find_elements(By.TAG_NAME, "article")
 
-    # 最初のツイートまでスクロール
-    actions = ActionChains(driver)
-    actions.move_to_element(tweet)
-    actions.perform()
-    time.sleep(1)
+    for tweet in tweets:
+        tweets = driver.find_elements(By.TAG_NAME, "article")
 
-    # ツイート元アカウント名の要素を取得しページ移動
-    account_name_elements = tweet.find_element(
-        By.XPATH, './/div[@data-testid="User-Name"]//a[@role="link"]'
-    )
-    follow_username = account_name_elements.get_attribute("href").split("/")[-1]
-    current_url = "https://twitter.com/" + follow_username
+        # 最初のツイートまでスクロール
+        actions = ActionChains(driver)
+        actions.move_to_element(tweet)
+        actions.perform()
+        time.sleep(1)
 
-    # 新しいタブを作成し、フォーカスする
-    driver.switch_to.new_window("tab")
-    driver.switch_to.window(driver.window_handles[1])
-    driver.get(current_url)
+        # ツイート元アカウント名の要素を取得しページ移動
+        account_name_elements = tweet.find_element(
+            By.XPATH, './/div[@data-testid="User-Name"]//a[@role="link"]'
+        )
+        print(account_name_elements.text)
+        follow_username = account_name_elements.get_attribute("href").split("/")[-1]
+        current_url = "https://twitter.com/" + follow_username
 
-    # ページ遷移の完了を待つ
-    wait.until(
-        EC.presence_of_all_elements_located(
-            (
-                By.XPATH,
-                '//div[@data-testid="primaryColumn"]//div[@data-testid="placementTracking"]',
+        # 新しいタブを作成し、フォーカスする
+        driver.switch_to.new_window("tab")
+        # タブのハンドルを取得
+        handles = driver.window_handles
+        driver.switch_to.window(handles[-1])
+        # driver.switch_to.window(driver.window_handles[1])
+        driver.get(current_url)
+
+        # ページ遷移の完了を待つ
+        wait.until(
+            EC.presence_of_all_elements_located(
+                (
+                    By.XPATH,
+                    '//div[@data-testid="primaryColumn"]//div[@data-testid="placementTracking"]',
+                )
             )
         )
-    )
 
-    time.sleep(3)
+        time.sleep(1)
 
-    follow_button = driver.find_element(
-        By.XPATH,
-        '//div[@data-testid="primaryColumn"]//div[@data-testid="placementTracking"]',
-    )
-
-    actions.move_to_element(follow_button)
-    actions.perform()
-    follow_button.click()
-
-    time.sleep(2)
-    driver.close()
-    time.sleep(2)
-    driver.switch_to.window(driver.window_handles[0])
-    time.sleep(2)
-
-    # ツイートをリツイートする
-    try:
-        retweet_button = tweet.find_element(By.XPATH, './/div[@data-testid="retweet"]')
-        actions.move_to_element(retweet_button)
-        actions.perform()
-        retweet_button.click()
-
-        time.sleep(2)
-
-        confirm_button = tweet.find_element(
-            By.XPATH, '//div[@data-testid="retweetConfirm"]'
+        follow_button = driver.find_element(
+            By.XPATH,
+            '//div[@data-testid="primaryColumn"]//div[@data-testid="placementTracking"]',
         )
-        actions.move_to_element(confirm_button)
+
+        actions.move_to_element(follow_button)
         actions.perform()
-        confirm_button.click()
+        follow_button.click()
 
-        time.sleep(2)
+        time.sleep(1)
+        driver.close()
+        time.sleep(1)
+        driver.switch_to.window(handles[0])
+        # driver.switch_to.window(driver.window_handles[0])
+        time.sleep(1)
 
-    except:
-        print("リツイート済み")
-        continue
+        # ツイートをリツイートする
+        try:
+            retweet_button = tweet.find_element(
+                By.XPATH, './/div[@data-testid="retweet"]'
+            )
+            actions.move_to_element(retweet_button)
+            actions.perform()
+            retweet_button.click()
+
+            time.sleep(1)
+
+            confirm_button = tweet.find_element(
+                By.XPATH, '//div[@data-testid="retweetConfirm"]'
+            )
+            actions.move_to_element(confirm_button)
+            actions.perform()
+            confirm_button.click()
+
+            time.sleep(1)
+
+        except:
+            print("リツイート済み")
+            continue
+
+    print("-------------ループ終わり-------------")
+    y = 0
+    y += 5000
+    driver.execute_script(f"window.scrollTo(0,{y})")
+    time.sleep(2)
+
+    new_tweets = driver.find_elements(By.TAG_NAME, "article")
+    time.sleep(2)
+    if new_tweets == tweets:
+        break
 
 driver.quit()
